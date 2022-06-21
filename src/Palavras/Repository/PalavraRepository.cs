42c338052987cfe90822e0825aa43709cd78f8a2
Flávio Júnior <flaviospjunior@gmail.com>
2022-06-20 21:18:28 -0300

    Repository implementation

--- src/Palavras/Repository/PalavraRepository.cs
+++ src/Palavras/Repository/PalavraRepository.cs
@@ -1,6 +1,29 @@
-﻿namespace Palavras.Repository
+﻿using Palavras.AppDbContext;
+using Palavras.Models;
+
+namespace Palavras.Repository
 {
-    public class PalavraRepository
+    public class PalavraRepository : IPalavraRepository
     {
+
+        MongoDbContext db = new MongoDbContext();
+
+        public async Task Add(PalavraModel palavraModel)
+        {
+            try
+            {
+                await db.
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+        public Task<PalavraModel> GetPalavra()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
